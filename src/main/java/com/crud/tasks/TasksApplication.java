1085106af73311961fea42dafcf5b732544525f0
“arkadyj <szelaga@gmail.com>
2018-01-03 19:40:43 +0100

    modul 14_3

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,19 +1,13 @@
 package com.crud.tasks;
 
-import com.crud.tasks.domain.Task;
-import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TasksApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		//SpringApplication.run(TasksApplication.class, args);
-
-
-
-
-	}
+        SpringApplication.run(TasksApplication.class, args);
+    }
 }
