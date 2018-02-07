d44585d6481f89af4223bf625a9dc6f528e16c7d
arkadyj <szelaga@gmail.com>
2018-02-07 14:49:50 +0100

    zad_19_4

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,46 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Task once a day email";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        LOGGER.info("Processing scheduled method");
+
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                null,
+                SUBJECT,
+                messageBuilder()
+        ));
+    }
+
+    private String messageBuilder() {
+        long size = taskRepository.count();
+        return (size == 1) ? "Database contains " + size + " task " : "Database contains " + size + " tasks ";
+    }
+}
--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -1,4 +1,44 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import com.crud.tasks.trello.config.AdminConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TrelloService {
+
+    private static final String SUBJECT = "Task new Trello card";
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+
+        Optional.ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                "szelaga@mcs-przychodnia",
+                SUBJECT,
+                "New Card" + card.getName() + "has been created")));
+
+        return newCard;
+    }
 }
