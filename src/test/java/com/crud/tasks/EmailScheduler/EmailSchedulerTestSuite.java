0bf5350c6fb8f3fd9a328dcb457950ab3acb4677
arkadyj <szelaga@gmail.com>
2018-03-02 14:46:37 +0100

    zad_21_2

--- src/test/java/com/crud/tasks/EmailScheduler/EmailSchedulerTestSuite.java
+++ src/test/java/com/crud/tasks/EmailScheduler/EmailSchedulerTestSuite.java
@@ -1,42 +1,42 @@
 package com.crud.tasks.EmailScheduler;
 
+import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.scheduler.EmailScheduler;
+import com.crud.tasks.trello.config.AdminConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mail.javamail.JavaMailSender;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class EmailSchedulerTestSuite {
 
-    @InjectMocks
-    private EmailScheduler emailScheduler;
+    @Mock
+    EmailScheduler emailScheduler;
 
-    @MockBean
+    @Mock
     private TaskRepository taskRepository;
 
     @Mock
-    private JavaMailSender javaMailSender;
+    private AdminConfig adminConfig;
 
     @Test
-    public void shouldMessageBuilder() {
+    public void shouldMailScheduler() {
         //Given
+        Mail mail = new Mail("test@gmail.com","","Test mail", "Message body");
+
+        when(taskRepository.count()).thenReturn(1L);
+        when(adminConfig.getAdminMail()).thenReturn("test@gmail.com");
 
         //When
-        //when(taskRepository.count()).thenReturn(5L);
-
-        //emailScheduler.sendInformationEmail();
+        emailScheduler.sendInformationEmail();
 
         //Then
-
-
+        verify(emailScheduler,times(1)).sendInformationEmail();
     }
-
-
 }
